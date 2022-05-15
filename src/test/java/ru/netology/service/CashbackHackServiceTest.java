b0f8e23e996faf2a7fb8836a6edfa4159734074c
OlesyaSergeevnaPopova <lesenok170389@rambler.ru>
2022-05-15 17:14:26 +0300

    Added test Zero Cash

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,11 +5,23 @@ import org.testng.Assert;
 
 
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
     @org.testng.annotations.Test
     public void cashback() {
-        CashbackHackService service = new CashbackHackService();
+
         int amount = 2000;
         int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    public void zeroCashback() {
+
+        int amount = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
 
         assertEquals(actual,expected);
